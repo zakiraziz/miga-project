7e462d277d2361c191f3ed474fd0a56a98cc3399
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2024-10-08 22:45:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,22 @@ def speak(text):
     engine.runAndWait()
 
 if __name__ == "__main__":
-    speak("Hey sir how may i help you")
+    speak("Initializing Jarivis....")
+    while True:
+        # Listen for the wake word "jarvis"  
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening....")
+            audio = r.listen(source)
+
+        command = r.recognize_sphinx(audio)
+        print(command)
+
+        # recognize speech using Sphinx
+        try:
+            print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
