d9c860e098c72422af1ec5e904004b54dc6815a8
Zakir Aziz <your-email@example.com>
2024-10-08 22:43:59 +0500

    speaking

---
+++ main.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Hey sir how may i help you")
